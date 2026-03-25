1718ae1df5ff17a3326c92bdf56abb42653494b1
vanilson muhongo <vanilsonmuhongo@outlook.pt>
2026-03-25 22:56:49 +0000

    init config project

--- src/test/java/code/with/vanilson/clinicflow/ClinicflowApplicationTests.java
+++ src/test/java/code/with/vanilson/clinicflow/ClinicflowApplicationTests.java
@@ -1,15 +1,13 @@
 package code.with.vanilson.clinicflow;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
 
-@Import(TestcontainersConfiguration.class)
-@SpringBootTest
 class ClinicflowApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+        Assertions.assertEquals(3, 1 + 2);
+    }
 
 }
